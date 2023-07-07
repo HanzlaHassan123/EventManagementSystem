c51977438988db97bee10ab182553b225a0c75cf
hp <tararhanzla783@gmail.co,m>
2023-07-07 11:37:31 -0700

    2nd commit

--- EventManagementSystem/Areas/Identity/Data/AuthDbContext.cs
+++ EventManagementSystem/Areas/Identity/Data/AuthDbContext.cs
@@ -10,6 +10,7 @@ public class AuthDbContext : IdentityDbContext<ApplicationUser>
     public AuthDbContext(DbContextOptions<AuthDbContext> options)
         : base(options)
     {
+
     }
 
     protected override void OnModelCreating(ModelBuilder builder)
